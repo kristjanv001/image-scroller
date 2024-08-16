6f9034f88647e25115f63884a75aca871bc58efd
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-08-16 23:20:14 +0300

    Hero rwd done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
-    colors: {
-      "neutral-one": "#F8FAFC",
-      "neutral-seven": "#475569",
-      "neutral-ten": "#0F172A",
-      "rose-seven": "#E11D48",
+    extend: {
+      colors: {
+        "neutral-one": "#F8FAFC",
+        "neutral-seven": "#475569",
+        "neutral-ten": "#0F172A",
+        "rose-seven": "#E11D48",
+      },
     },
+
     fontFamily: {
       sans: "Roboto, system-ui",
       serif: "'Roboto Slab', Georgia",
