a5de28fab0eab558b08a8ec64500838ec92cd724
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-08-16 23:38:23 +0300

    Filter styate change aka active filter done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { GalleryService } from "./services/gallery.service";
 import { Filter } from "./models/filter";
@@ -6,7 +7,7 @@ import { Filter } from "./models/filter";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
