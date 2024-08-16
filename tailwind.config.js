9c7e674e255f25c21c7bc36262bd15acb8209ff9
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-08-16 20:48:56 +0300

    Colors done, typography done, fonts done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    colors: {
+      "neutral-one": "#F8FAFC",
+      "neutral-seven": "#475569",
+      "neutral-ten": "#0F172A",
+      "rose-seven": "#E11D48",
+    },
+    fontFamily: {
+      sans: "Roboto, system-ui",
+      serif: "'Roboto Slab', Georgia",
+    },
   },
   plugins: [],
 };
